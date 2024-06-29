609406c6f92c5776e761a526c963f8d1b052c152
Krystian Szczegielniak <k.szczegielniak@protonmail.com>
2024-06-29 20:51:25 +0200

    Add movement.

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -3,13 +3,18 @@ using Godot;
 
 public partial class Player : CharacterBody3D
 {
+    private Vector2 direction = new();
+
     public override void _PhysicsProcess(double delta)
     {
-        GD.Print("Player _PhysicsProcess");
+        Velocity = new(direction.X, 0f, direction.Y);
+        Velocity *= 5;
+
+        MoveAndSlide();
     }
 
     public override void _Input(InputEvent @event)
     {
-        GD.Print("Input" + @event);
+        direction = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
     }
 }
